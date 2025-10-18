72b6030afbf8ae917e84d8b8ef14586f7be4478c
jacksonpacek <jackson.pacek@gmail.com>
2025-10-18 18:24:26 -0400

    clock finished with added get methods

--- clock.py
+++ clock.py
@@ -4,18 +4,18 @@ from time import strftime
 
 class Clock:
     def __init__(self):
-        self.fastTime = strftime("%H:%M:%S")
-        self.incTimer = Timer(0.1, self.incTime)
-        self.incTimer.start()
+        self._fastTime = strftime("%H:%M:%S")
+        self._incTimer = Timer(0.1, self._incTime)
+        self._incTimer.start()
 
 
-    def incTime(self):
-        self.incTimer = Timer(0.1, self.incTime)
-        self.incTimer.start()
+    def _incTime(self):
+        self._incTimer = Timer(0.1, self._incTime)
+        self._incTimer.start()
 
-        hours = int(self.fastTime[:2])
-        mins = int(self.fastTime[3:5])
-        secs = int(self.fastTime[6:])
+        hours = int(self._fastTime[:2])
+        mins = int(self._fastTime[3:5])
+        secs = int(self._fastTime[6:])
 
         secs += 1
         #increment fast time by 1 second every 100 ms
@@ -31,5 +31,13 @@ class Clock:
                 if (hours == 24):
                     hours = 0
 
-        self.fastTime = f"{hours:02d}:{mins:02d}:{secs:02d}"
-        print(self.fastTime)
+        self._fastTime = f"{hours:02d}:{mins:02d}:{secs:02d}"
+        print(self._fastTime)
+
+    
+    def getTime(self):
+        return self._fastTime
+    
+
+    def getTimeNoSecs(self):
+        return self._fastTime[:5]
