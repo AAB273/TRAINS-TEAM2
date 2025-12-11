fa7f708fa08c2943bdf465af9d5cabfe8995744e
Yeabsira.Frost <yef7@pitt.edu>
2025-12-11 11:07:45 -0500

    running

--- launch_all_modules.py
+++ launch_all_modules.py
@@ -43,11 +43,11 @@ def launch_all():
         ("Track SW", "Wayside_Controller/SW/main.py"),
         ("Track HW", "Wayside_Controller/HW/WC_HW_MainUI.py"),
         ("Track Model", "Track Model/UI_Structure.py"),
-        ("Train Model", "Train Model/Passenger_UI.py"),
-        ("Train SW", "train_controller_sw/Driver_UI.py"),
-        ("Train HW","HW_Train_Controller/TC_HW_MainUI.py"),
+        # ("Train Model", "Train Model/Passenger_UI.py"),
+        # ("Train SW", "train_controller_sw/Driver_UI.py"),
+        # ("Train HW","HW_Train_Controller/TC_HW_MainUI.py"),
         
-        ("Test UI","Train Model/Test_UI.py")
+        # ("Test UI","Train Model/Test_UI.py")
     ]
     
     # Set PYTHONPATH to include exe_dir so imports work
